a440ad0fda790841ad4e7edba5afe619fa2ae27b
Michel Zimmer <opensource@michel-zimmer.de>
2022-04-19 22:15:19 +0200

    wip

--- fn4j-http-client-apachehc/src/test/java/fn4j/http/client/apachehc/ApacheHcClientTest.java
+++ fn4j-http-client-apachehc/src/test/java/fn4j/http/client/apachehc/ApacheHcClientTest.java
@@ -3,62 +3,74 @@ package fn4j.http.client.apachehc;
 import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
 import fn4j.http.core.Body;
 import fn4j.http.core.Method;
-import fn4j.http.core.Request;
 import fn4j.http.core.Response;
-import fn4j.http.core.header.BearerAuthenticationHeader;
-import fn4j.http.core.header.HeaderName;
-import fn4j.http.core.header.HeaderValue;
-import fn4j.http.core.header.RawHeader;
+import fn4j.http.core.header.Headers;
+import fn4j.net.uri.Path;
+import fn4j.net.uri.Query;
 import fn4j.net.uri.Uri;
+import fn4j.net.uri.UriComponent;
+import io.vavr.collection.Stream;
 import io.vavr.concurrent.Future;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.RegisterExtension;
+import net.jqwik.api.ForAll;
+import net.jqwik.api.Label;
+import net.jqwik.api.Property;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 import static fn4j.http.core.Request.request;
-import static fn4j.http.core.header.BearerAuthenticationHeader.Token.token;
-import static fn4j.http.core.header.Headers.headers;
+import static fn4j.net.uri.Literal.QUESTION_MARK;
 import static io.vavr.API.TODO;
-import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.assertj.vavr.api.VavrAssertions.assertThat;
 
+@Label("Apache HTTP Components Client")
 class ApacheHcClientTest {
-    @RegisterExtension
-    WireMockExtension server = WireMockExtension.newInstance()
-                                                .options(wireMockConfig().dynamicPort())
-                                                .failOnUnmatchedRequests(true)
-                                                .build();
-
-    @Test
-    void shouldExchangeRequestAndResponse() throws Exception {
+    @Property
+    @Label("should exchange request and response")
+    void shouldExchangeRequestAndResponse(@ForAll Method method,
+                                          @ForAll Path path,
+                                          @ForAll Query query,
+                                          @ForAll Headers headers,
+                                          @ForAll byte[] body) throws Exception {
         // given
-        try (var apacheHcClient = ApacheHcClient.builder().build()) {
-            Request<byte[]> request = request(Method.POST,
-                                              new Uri(server.url("/path?parameter=value&flag")),
-                                              headers(new RawHeader(new HeaderName("name"),
-                                                                    new HeaderValue("value")),
-                                                      new BearerAuthenticationHeader(token("token"))),
-                                              new Body<>("Body".getBytes(UTF_8)));
+        WireMockExtension server = WireMockExtension.newInstance()
+                                                    .options(wireMockConfig().dynamicPort())
+                                                    .failOnUnmatchedRequests(true)
+                                                    .build();
+        try {
+            var pathAndQueryString = Stream.<UriComponent>empty()
+                                           .appendAll(path.pathSegments())
+                                           .append(QUESTION_MARK)
+                                           .append(query)
+                                           .map(UriComponent::encode)
+                                           .mkString();
+            var uri = new Uri(server.url(pathAndQueryString));
+            var request = request(method,
+                                  uri,
+                                  headers,
+                                  new Body<>(body));
 
-            server.givenThat(
-                    post(urlEqualTo("/path?parameter=value&flag"))
-                            .withHeader("name", equalTo("value"))
-                            .withHeader("Authentication", equalTo("Bearer token"))
-                            .withRequestBody(equalTo("Body"))
-                            .willReturn(
-                                    aResponse().withBody(new byte[]{(byte) 0x80})
-                            )
-            );
+            try (var apacheHcClient = ApacheHcClient.builder().build()) {
+                server.givenThat(
+                        post(urlEqualTo(pathAndQueryString))
+                                .withHeader("name", equalTo("value"))
+                                .withHeader("Authentication", equalTo("Bearer token"))
+                                .withRequestBody(equalTo("Body"))
+                                .willReturn(
+                                        aResponse().withBody(new byte[]{(byte) 0x80})
+                                )
+                );
 
-            // when
-            Future<Response<byte[]>> eventualResponse = apacheHcClient.exchange(request);
+                // when
+                Future<Response<byte[]>> eventualResponse = apacheHcClient.exchange(request);
 
-            // then
-            assertThat(eventualResponse.await(2, SECONDS).toTry()).isSuccess().hasValueSatisfying(response -> {
-                TODO();
-            });
+                // then
+                assertThat(eventualResponse.await(2, SECONDS).toTry()).isSuccess().hasValueSatisfying(response -> {
+                    TODO();
+                });
+            }
+        } finally {
+            server.shutdownServer();
         }
     }
 }
--- fn4j-net-uri/src/main/java/fn4j/net/uri/Literal.java
+++ fn4j-net-uri/src/main/java/fn4j/net/uri/Literal.java
@@ -1,14 +1,14 @@
 package fn4j.net.uri;
 
-record Literal(String value) implements UriComponent {
-    static final Literal COLON = new Literal(":");
-    static final Literal DOUBLE_SLASH = new Literal("//");
-    static final Literal AT = new Literal("@");
-    static final Literal SLASH = new Literal("/");
-    static final Literal QUESTION_MARK = new Literal("?");
-    static final Literal EQUALS = new Literal("=");
-    static final Literal AMPERSAND = new Literal("&");
-    static final Literal HASH = new Literal("#");
+public record Literal(String value) implements UriComponent {
+    public static final Literal COLON = new Literal(":");
+    public static final Literal DOUBLE_SLASH = new Literal("//");
+    public static final Literal AT = new Literal("@");
+    public static final Literal SLASH = new Literal("/");
+    public static final Literal QUESTION_MARK = new Literal("?");
+    public static final Literal EQUALS = new Literal("=");
+    public static final Literal AMPERSAND = new Literal("&");
+    public static final Literal HASH = new Literal("#");
 
     @Override
     public String encode() {
